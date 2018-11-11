b94292e56137e086ea035ee59fd142219d511ffe
jinuman <jin35kim@gmail.com>
2018-11-11 16:53:05 +0900

    test: Refactor entry test code

--- DailyJournalTests/EntryTests.swift
+++ DailyJournalTests/EntryTests.swift
@@ -9,17 +9,23 @@
 import XCTest
 @testable import DailyJournal
 
+extension Entry {
+    static var dayBeforeYesterday: Entry { return Entry(id: UUID(), createdAt: Date.distantPast, text: "그저께 일기") }
+    static var yesterDay: Entry { return Entry(id: UUID(), createdAt: Date(), text: "어제 일기") }
+    static var today: Entry { return Entry(id: UUID(), createdAt: Date.distantFuture, text: "오늘 일기") }
+}
+
 class EntryTests: XCTestCase {
 
     func testEditEntryText() {
         // Setup
-        let entry = Entry(text: "hello")
+        let entry = Entry.today
         
         // Run
-        entry.text = "first journal"
+        entry.text = "hello"
         
         // Verify
-        XCTAssertEqual(entry.text, "first journal")
+        XCTAssertEqual(entry.text, "hello")
     }
     
     func testEntryEqual() {
@@ -93,9 +99,9 @@ class EntryTests: XCTestCase {
     
     func test_최근_순으로_엔트리를_불러올_수_있다() {
         // Setup
-        let dayBeforeYesterday = Entry(id: UUID(), createdAt: Date.distantPast, text: "그저께 일기")
-        let yesterDay = Entry(id: UUID(), createdAt: Date(), text: "어제 일기")
-        let today = Entry(id: UUID(), createdAt: Date.distantFuture, text: "오늘 일기")
+        let dayBeforeYesterday = Entry.dayBeforeYesterday
+        let yesterDay = Entry.yesterDay
+        let today = Entry.today
         
         let journal = InMemoryEntryRepository(entriesArray: [dayBeforeYesterday, yesterDay, today])
         
@@ -109,9 +115,9 @@ class EntryTests: XCTestCase {
     
     func test_요청한_엔트리의_수만큼_최신_순으로_반환한다() {
         // Setup
-        let dayBeforeYesterday = Entry(id: UUID(), createdAt: Date.distantPast, text: "그저께 일기")
-        let yesterDay = Entry(id: UUID(), createdAt: Date(), text: "어제 일기")
-        let today = Entry(id: UUID(), createdAt: Date.distantFuture, text: "오늘 일기")
+        let dayBeforeYesterday = Entry.dayBeforeYesterday
+        let yesterDay = Entry.yesterDay
+        let today = Entry.today
         
         let journal = InMemoryEntryRepository(entriesArray: [dayBeforeYesterday, yesterDay, today])
         
@@ -125,9 +131,9 @@ class EntryTests: XCTestCase {
     
     func test_존재하는_엔트리보다_많은_수를_요청하면_존재하는_엔트리만큼만_반환한다() {
         // Setup
-        let dayBeforeYesterday = Entry(id: UUID(), createdAt: Date.distantPast, text: "그저께 일기")
-        let yesterDay = Entry(id: UUID(), createdAt: Date(), text: "어제 일기")
-        let today = Entry(id: UUID(), createdAt: Date.distantFuture, text: "오늘 일기")
+        let dayBeforeYesterday = Entry.dayBeforeYesterday
+        let yesterDay = Entry.yesterDay
+        let today = Entry.today
         
         let journal = InMemoryEntryRepository(entriesArray: [dayBeforeYesterday, yesterDay, today])
         
