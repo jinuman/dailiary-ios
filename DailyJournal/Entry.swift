2a0b4fc5833bfffad14a9e7f4ffe62a32fbe8303
jinuman <jin35kim@gmail.com>
2018-11-11 14:12:07 +0900

    feat: Add entry model

---
+++ DailyJournal/Entry.swift
@@ -0,0 +1,21 @@
+//
+//  Entry.swift
+//  DailyJournal
+//
+//  Created by Jinwoo Kim on 2018. 11. 11..
+//  Copyright © 2018년 jinuman. All rights reserved.
+//
+
+import Foundation
+
+class Entry {
+    let id: UUID
+    let createdAt: Date
+    var text: String
+    
+    init(id: UUID = UUID(), createdAt: Date = Date(), text: String) {
+        self.id = id
+        self.createdAt = createdAt
+        self.text = text
+    }
+}
