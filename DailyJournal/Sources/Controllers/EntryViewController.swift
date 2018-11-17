a7886d82cd622906c79ccd5dcbce89e87cfca8fa
jinuman <jin35kim@gmail.com>
2018-11-17 14:42:34 +0900

    feat: Add EntryViewController and import SnapKit

---
+++ DailyJournal/Sources/Controllers/EntryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EntryViewController.swift
+//  DailyJournal
+//
+//  Created by Jinwoo Kim on 2018. 11. 17..
+//  Copyright © 2018년 jinuman. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class EntryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
