3b328612f805886151f3f3afdde302a7ee5dbbdc
jinuman <jin35kim@gmail.com>
2018-11-17 15:15:30 +0900

    feat: Add textView and long text for test

--- DailyJournal/Sources/Controllers/EntryViewController.swift
+++ DailyJournal/Sources/Controllers/EntryViewController.swift
@@ -9,8 +9,23 @@
 import UIKit
 import SnapKit
 
+let tmpTxt = """
+There are five people in my family and I’m the second youngest.
+I’m living in Bundang with my father and mother at the moment.
+My older sister is living in Incheon since she had married.
+My mother is doctor, as well as my father.
+And I'm seeking for a job at the moment.
+From now on, I'll tell you about my hobbies
+Normally in my time, I’m doing iOS programming as a hobby.
+And I like to ride bicycle and play guitar.
+Most of my time I'm alone.
+But I’m not lonely because I’m busy developing iOS App for my own.
+I want to get a job as an iOS developer as soon as possible.
+"""
+
 class EntryViewController: UIViewController {
     let headerView: UIView = UIView()
+    let textView: UITextView = UITextView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,10 +34,13 @@ class EntryViewController: UIViewController {
         layout()
         
         headerView.backgroundColor = UIColor.init(red: 0.909, green: 0.510, blue: 0.488, alpha: 1.0)
+        textView.text = tmpTxt
+        textView.font = UIFont.systemFont(ofSize: 50)
     }
     
     private func addSubviews() {
         view.addSubview(headerView)
+        view.addSubview(textView)
     }
     
     private func layout() {
@@ -30,7 +48,11 @@ class EntryViewController: UIViewController {
             $0.leading.top.trailing.equalToSuperview()
             $0.height.equalTo(100)
         }
+        
+        textView.snp.makeConstraints {
+            $0.top.equalTo(headerView.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
     }
-    
     
 }
