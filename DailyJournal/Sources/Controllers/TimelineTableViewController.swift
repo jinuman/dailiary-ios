a39a7e201d0c05b3872a33f3b856d9f97b1da383
jinuman <jin35kim@gmail.com>
2018-12-19 16:10:02 +0900

    feat: Add tableview for timeline

--- DailyJournal/Sources/Controllers/TimelineTableViewController.swift
+++ DailyJournal/Sources/Controllers/TimelineTableViewController.swift
@@ -15,6 +15,18 @@ class TimelineTableViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+}
 
+extension TimelineTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TimelineCell", for: indexPath)
+        cell.backgroundColor = .purple
+        return cell
+    }
+    
 }
 
