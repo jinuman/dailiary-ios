ec1c37a086117c6c67fd86a4f7003c7a5fc82778
jinuman <jin35kim@gmail.com>
2018-12-19 18:04:08 +0900

    feat: Implement saveEdit button action

--- DailyJournal/Sources/Controllers/EntryViewController.swift
+++ DailyJournal/Sources/Controllers/EntryViewController.swift
@@ -29,6 +29,9 @@ class EntryViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var textViewBottomConstraint: NSLayoutConstraint!
     
+    let repo = InMemoryEntryRepository.shared
+    private var editingEntry: Entry?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,5 +39,47 @@ class EntryViewController: UIViewController {
         textView.font = UIFont.systemFont(ofSize: 40)
         textView.text = longtxt
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateSubviews(isEditing: false)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    @objc func saveEntry(_ sender: UIBarButtonItem) {
+        if let editing = editingEntry {
+            editing.text = textView.text
+            repo.update(editing)
+        } else {
+            let entry: Entry = Entry(text: textView.text)
+            repo.add(entry)
+            editingEntry = entry
+        }
+        updateSubviews(isEditing: false)
+    }
+    
+    @objc func editEntry(_ sender: UIBarButtonItem) {
+        updateSubviews(isEditing: true)
+    }
+    
+    fileprivate func updateSubviews(isEditing: Bool) {
+        if isEditing {
+            textView.isEditable = true
+            textView.becomeFirstResponder()
+            
+            saveEditButton.image = #imageLiteral(resourceName: "baseline_save_white_24pt")
+            saveEditButton.target = self
+            saveEditButton.action = #selector(saveEntry(_:))
+        } else {
+            textView.isEditable = false
+            textView.resignFirstResponder()
+            
+            saveEditButton.image = #imageLiteral(resourceName: "baseline_edit_white_24pt")
+            saveEditButton.target = self
+            saveEditButton.action = #selector(editEntry(_:))
+        }
+    }
 }
