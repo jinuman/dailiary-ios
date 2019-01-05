d13d059a13ab297542597e4af8dd11fcacfbe66b
jinuman <jin35kim@gmail.com>
2019-01-05 21:57:34 +0900

    feat: Apply UserDefaults to save setting option

---
+++ DailyJournal/Sources/Model/UserDefaultsSettings.swift
@@ -0,0 +1,48 @@
+//
+//  UserDefaultsSettings.swift
+//  DailyJournal
+//
+//  Created by Jinwoo Kim on 2019. 1. 5..
+//  Copyright © 2019년 jinuman. All rights reserved.
+//
+
+import UIKit
+
+private let dateFormatOptionKey: String = "dateFormatOptionKey"
+private let fontSizeOptionKey: String = "fontSizeOptionKey"
+
+extension UserDefaults: Settings {
+    var dateFormatOption: DateFormatOption {
+        get {
+            let rawValue = object(forKey: dateFormatOptionKey) as? String
+            if
+                let rawValue = rawValue,
+                let option = DateFormatOption(rawValue: rawValue) {
+                return option
+            } else {
+                return DateFormatOption.default
+            }
+        }
+        set {
+            set(newValue.rawValue, forKey: dateFormatOptionKey)
+            synchronize()
+        }
+    }
+    
+    var fontSizeOption: FontSizeOption {
+        get {
+            let rawValue = object(forKey: fontSizeOptionKey) as? CGFloat
+            if
+                let rawValue = rawValue,
+                let option = FontSizeOption(rawValue: rawValue) {
+                return option
+            } else {
+                return FontSizeOption.default
+            }
+        }
+        set {
+            set(newValue.rawValue, forKey: fontSizeOptionKey)
+            synchronize()
+        }
+    }
+}
